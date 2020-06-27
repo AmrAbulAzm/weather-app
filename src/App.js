80f9e9575aa4e1a4277a89c5d292116ae21d0764
Amr Abulazm <amr.abulazm@gmail.com>
2020-06-27 16:16:43 +0200

    backbone, font, styled-components, assets

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Container>
+      <h1>Weather-app</h1>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  text-align: center;
+`
+
+export default App;
